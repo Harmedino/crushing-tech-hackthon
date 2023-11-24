20b0783f19e2d611865d3d6e94bbf5284e2ef346
Harmedino <adamsahmed191@gmail.com>
2023-11-24 15:42:30 +0100

    moving with the accordion

--- app.js
+++ app.js
@@ -83,3 +83,46 @@ upDown.addEventListener('click', () => {
   // Focus on the button to provide better navigation for screen reader users
   upDown.focus();
 });
+
+
+function rotateAndReplace(svgElement) {
+  // Rotate the SVG
+  svgElement.classList.toggle('rotate');
+
+  // Replace with a different SVG after 2 seconds
+  
+    svgElement.innerHTML = `
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="10" fill="#4A4A4A"></circle>
+    <path
+      d="M17.2738 8.52629C17.6643 8.91682 17.6643 9.54998 17.2738 9.94051L11.4405 15.7738C11.05 16.1644 10.4168 16.1644 10.0263 15.7738L7.3596 13.1072C6.96908 12.7166 6.96908 12.0835 7.3596 11.693C7.75013 11.3024 8.38329 11.3024 8.77382 11.693L10.7334 13.6525L15.8596 8.52629C16.2501 8.13577 16.8833 8.13577 17.2738 8.52629Z"
+      fill="#fff"
+    ></path>
+  </svg>
+    `;
+ 
+}
+function toggleList(listElement) {
+  const lists = document.querySelectorAll('.lists');
+  lists.forEach((list) => {
+    if (list !== listElement && list.classList.contains('active')) {
+      list.classList.remove('active');
+    }
+    listElement.classList.add('active');
+  });
+
+  // if (listElement.classList.contains('active')) {
+  //   return
+  // }
+
+  // Toggle the clicked list
+ 
+
+  // Rotate the arrow
+  // const rotateElement = listElement.querySelector('.rotate');
+  // rotateElement.classList.toggle('rotate');
+
+  // Close other lists
+ 
+}
+
