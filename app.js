e1142d9416d938dee95ddac92910caaaab43679d
Harmedino <adamsahmed191@gmail.com>
2023-11-30 21:02:55 +0100

    submitted already

--- app.js
+++ app.js
@@ -14,39 +14,39 @@ const successDiv = document.querySelectorAll(".success");
 const dropdownItems = profileDropdown.querySelectorAll('[role="menuitem"]');
 let progressValue = 0;
 let completedCount = 0;
-const progressBar = document.getElementById('progressBar');
-const progressLabel = document.querySelector('.bar');
-const checkbox = document.querySelectorAll('.checkbox')
-const checkboxStatus = document.querySelectorAll('.checkbox-status')
+const progressBar = document.getElementById("progressBar");
+const progressLabel = document.querySelector(".bar");
+const checkbox = document.querySelectorAll(".checkbox");
+const checkboxStatus = document.querySelectorAll(".checkbox-status");
+const profileItem = document.querySelectorAll('.focus')
 
 function toggleProfileDropdown() {
-  const isHidden = profileDropdown.classList.contains("hidden");
+  const isHidden = profileDropdown.classList.contains("hide");
   const expanded = notificationIcon.getAttribute("aria-expanded") === "true";
 
-  if (!alertBell.classList.contains("hidden")) {
-    alertBell.classList.add("hidden");
+  if (!alertBell.classList.contains("hide")) {
+    alertBell.classList.add("hide");
   }
   // Toggle the visibility of the dropdown
 
-  profileDropdown.classList.toggle("hidden");
-
+  profileDropdown.classList.toggle("hide");
+  profileItem[0].focus()
 
   profileName.setAttribute("aria-expanded", isHidden ? "true" : "false");
-
 }
 profileName.addEventListener("click", toggleProfileDropdown);
 
 function toggleNotificationIcon() {
-  const isDropdownHidden = alertBell.classList.contains("hidden");
+  const isDropdownHidden = alertBell.classList.contains("hide");
 
-  if (!profileDropdown.classList.contains("hidden")) {
-    profileDropdown.classList.add("hidden");
+  if (!profileDropdown.classList.contains("hide")) {
+    profileDropdown.classList.add("hide");
   }
   if (isDropdownHidden) {
-    alertBell.classList.toggle("hidden");
+    alertBell.classList.toggle("hide");
   } else {
-    profileDropdown.classList.add("hidden");
-    alertBell.classList.toggle("hidden");
+    profileDropdown.classList.add("hide");
+    alertBell.classList.toggle("hide");
   }
 
   // Update aria-expanded attribute for the notification icon
@@ -57,9 +57,8 @@ function toggleNotificationIcon() {
 
   notificationIcon.focus();
 
-   
   const isExpanded = alertBell.getAttribute("aria-expanded") === "true";
-  
+
   if (isExpanded) {
     alertBell.setAttribute("aria-expanded", "false");
     alertBell.setAttribute("aria-live", "off");
@@ -70,8 +69,6 @@ function toggleNotificationIcon() {
     alertBell.setAttribute("aria-label", "Notification open");
   }
 }
-
-
 
 // Call this function when you want to announce an alert
 // announceAlert('New alert message here');
@@ -84,26 +81,28 @@ document.addEventListener("keydown", (event) => {
   if (focusedElement.classList.contains("profileName")) {
     if (event.key === "Enter") {
       toggleProfileDropdown();
-      if (focusedElement.classList.contains("cancel")) {
-        trialPlan.classList.add("hidden");
-      }
+     
     }
+  } if (event.key === "Enter" && focusedElement.classList.contains("cancel" )) {
+    trialPlan.classList.add("hidden");
+    
   }
-  
-  if (event.key === "Escape" && !profileDropdown.classList.contains("hidden")) {
-    profileDropdown.classList.add("hidden");
+
+  if (event.key === "Escape" && !profileDropdown.classList.contains("hide")) {
+    profileDropdown.classList.add("hide");
   }
   if (focusedElement.classList.contains("profile")) {
     if (event.key === "Enter") {
       toggleNotificationIcon();
     }
   }
-  if (event.key === "Escape" && !alertBell.classList.contains("hidden")) {
-    alertBell.classList.add("hidden");
-  } if (focusedElement.classList.contains('list-head')) {
-    if (event.key === 'Enter') {
-      const listHead = focusedElement.closest('.list-head');
-      toggleList(listHead)
+  if (event.key === "Escape" && !alertBell.classList.contains("hide")) {
+    alertBell.classList.add("hide");
+  }
+  if (focusedElement.classList.contains("list-head")) {
+    if (event.key === "Enter") {
+      const listHead = focusedElement.closest(".list-head");
+      toggleList(listHead);
     }
   }
 });
@@ -112,16 +111,14 @@ trialCallout.addEventListener("click", () => {
   trialPlan.classList.add("hidden");
 });
 
-
-
 function toggleUpDown() {
-  upArrow.classList.toggle("hidden");
-  downArrow.classList.toggle("hidden");
+  upArrow.classList.toggle("hide2");
+  downArrow.classList.toggle("hide2");
 
   const expanded = upDown.getAttribute("aria-expanded") === "true";
 
   // Toggle visibility of the content
-  stepsContainer.classList.toggle("hidden", !expanded);
+  stepsContainer.classList.toggle("hide", !expanded);
 
   // Toggle aria-expanded attribute
   upDown.setAttribute("aria-expanded", String(!expanded));
@@ -140,8 +137,6 @@ upDown.addEventListener("keydown", (event) => {
   }
 });
 
-
-
 function toggleList(listElement) {
   const lists = document.querySelectorAll(".lists");
   const imgBox = document.querySelectorAll(".img-box");
@@ -149,10 +144,10 @@ function toggleList(listElement) {
   lists.forEach((list, index) => {
     const currentImgBox = imgBox[index];
 
-    if (list === listElement.closest('.lists')) {
+    if (list === listElement.closest(".lists")) {
       if (currentImgBox.classList.contains("hidden")) {
         // Close all other img-box elements
-        imgBox.forEach(box => box.classList.add("hidden"));
+        imgBox.forEach((box) => box.classList.add("hidden"));
 
         // Open the selected img-box
         currentImgBox.classList.remove("hidden");
@@ -166,24 +161,18 @@ function toggleList(listElement) {
   });
 }
 
-
-
-
-
 // Function to handle the logic inside the click event
 function handleRotateClick(index) {
-
   // Simulate loading state
   rotateDiv[index].setAttribute("aria-checked", "false");
   rotateDiv[index].setAttribute("aria-label", "Item not marked");
 
   rotateDiv[index].style.display = "none";
-  loadingDiv[index].style.display = "block";
+  loadingDiv[index].style.display = "inherit";
   loadingDiv[index].setAttribute("aria-hidden", "false");
   successDiv[index].setAttribute("aria-hidden", "true");
-  checkboxStatus[index].ariaLabel = 'Loading please wait'
+  checkboxStatus[index].ariaLabel = "Loading please wait";
   setTimeout(() => {
-    
     // Simulate success state
     loadingDiv[index].style.display = "none";
     successDiv[index].style.display = "block";
@@ -195,7 +184,7 @@ function handleRotateClick(index) {
 
     // Update progress
     progressValue += 20; // Adjust this value based on your progress logic
-    progressBar.setAttribute('width', progressValue);
+    progressBar.setAttribute("width", progressValue);
     progressBar.setAttribute("aria-valuenow", progressValue);
     progressBar.setAttribute("aria-valuetext", `${progressValue}%`);
     progressBar.setAttribute("aria-labelledby", "completionStatus");
@@ -205,17 +194,19 @@ function handleRotateClick(index) {
 
     if (index < loadingDiv.length) {
       toggleList(loadingDiv[index + 1].closest(".list-head"));
-      loadingDiv[index + 1].closest('.checkbox').focus();
+      loadingDiv[index + 1].closest(".checkbox").focus();
     }
-    checkboxStatus[index].ariaLabel = 'successfully marked '
+    checkboxStatus[index].ariaLabel = "successfully marked ";
   }, 2000);
 }
 
 rotateDiv.forEach((rotate, index) => {
   rotate.addEventListener("click", () => {
-    
-    checkbox[index].ariaLabel = checkbox[index].ariaLabel.replace('mark', 'unmark')
-    
+    checkbox[index].ariaLabel = checkbox[index].ariaLabel.replace(
+      "mark",
+      "unmark"
+    );
+
     handleRotateClick(index);
   });
 
@@ -228,19 +219,15 @@ rotateDiv.forEach((rotate, index) => {
 
 successDiv.forEach((element, index) => {
   element.addEventListener("click", () => {
-    handleSuccessClick(index);
-    checkbox[index].ariaLabel = checkbox[index].ariaLabel.replace('unmark','mark')
-    handleRotateClick(index);
-  });
-
-  element.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      handleSuccessClick(index);
-    }
+    handleSuccessClick(index, element);
+    checkbox[index].ariaLabel = checkbox[index].ariaLabel.replace(
+      "unmark",
+      "mark"
+    );
   });
 });
 
-function handleSuccessClick(index) {
+function handleSuccessClick(index, element) {
   element.style.display = "none";
   loadingDiv[index].style.display = "inherit";
   rotateDiv[index].setAttribute("aria-checked", "false");
@@ -249,24 +236,65 @@ function handleSuccessClick(index) {
   loadingDiv[index].setAttribute("aria-hidden", "false");
   successDiv[index].setAttribute("aria-hidden", "true");
 
-  
-  
-  checkboxStatus[index].ariaLabel = 'Loading please wait'
-  setTimeout(() => {
-    // Update progress
-    progressValue -= 20; // Adjust this value based on your progress logic
-  progressBar.setAttribute('width', progressValue);
-  progressBar.setAttribute("aria-valuenow", progressValue);
-  progressBar.setAttribute("aria-valuetext", `${progressValue}%`);
-  progressBar.setAttribute("aria-labelledby", "completionStatus");
+  console.log(element);
 
-  // Update completed count and progress label
-  completedCount -= 1;
-  progressLabel.textContent = `${completedCount} / 5 completed`;
+  checkboxStatus[index].ariaLabel = "Loading please wait";
+  setTimeout(() => {
     loadingDiv[index].style.display = "none";
+    // Update progress
+    progressValue -= 20;
+    progressBar.setAttribute("width", progressValue);
+    progressBar.setAttribute("aria-valuenow", progressValue);
+    progressBar.setAttribute("aria-valuetext", `${progressValue}%`);
+    progressBar.setAttribute("aria-labelledby", "completionStatus");
+
+    // Update completed count and progress label
+    completedCount -= 1;
+    progressLabel.textContent = `${completedCount} / 5 completed`;
+
     rotateDiv[index].style.display = "inherit";
-    checkboxStatus[index].ariaLabel = 'successfully unmark'
+    checkboxStatus[index].ariaLabel = "successfully unmark";
   }, 2000);
 }
 
+checkbox.forEach((element, index) => {
+  element.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      if (rotateDiv[index].style.display !== "none") {
+        handleRotateClick(index);
+      } else if (!successDiv[index].style.display !== "none") {
+        console.log(successDiv[index]);
+        handleSuccessClick(index, successDiv[index]);
+      }
+    }
+  });
+});
+
+function handleKeyPress(event, index) {
+  const lastItem = index === profileItem.length - 1
+  const firstItem = profileItem === 0
+  const nextItem = profileItem[index + 1]
+  const prevItem = profileItem[index - 1]
+  if (event.key == 'ArrowRight' || event.key === 'ArrowDown') {
+   
+    if (lastItem) {
+      profileItem[0].focus()
+      return;
+    }
+    nextItem.focus()
+  }
+  if (event.key == 'ArrowUp' || event.key == 'Arrowleft') {
+    if (firstItem) {
+      profileItem[profileItem.length - 1].focus()
+      return
+    }
+    prevItem.focus()
+  }
+}
+
+profileItem.forEach((element,index) => {
+  element.addEventListener('keyup', function (event) {
+    handleKeyPress(event,index)
+  })
+})
 
